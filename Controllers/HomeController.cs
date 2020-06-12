b63830849d084faa70f9bf74cd14cf80290376ea
Abhineel Rai <abhineel2707@gmail.com>
2020-06-12 20:30:16 +0530

    Adding Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuthClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            return View();
+        }
+    }
+}
